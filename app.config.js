79eedaee36b8149c7cc8a943a1f125fb457120af
cliffordmarkmusni <cliffordmarkmusni@gmail.com>
2026-02-17 06:54:15 +0800

    Update API_BASE_URL to Railway production URL

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ module.exports = ({ config }) => {
   return {
     ...config,
     extra: {
-      API_BASE_URL: process.env.API_BASE_URL || "http://localhost:3000",
+      API_BASE_URL: process.env.API_BASE_URL || "https://portionist-production.up.railway.app",
     },
   };
 };
